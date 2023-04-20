ab41b4403387e6fecea26ed53d08fe298e8e39bb
Hyun Seok Park <luckseok1@gmail.com>
2023-04-20 21:49:27 +0900

    feat: chakra_ui 메인 뷰 작업

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,22 @@
 import { NextPage } from 'next';
+import { Box, Center, Flex, Heading } from '@chakra-ui/react';
+import { ServiceLayout } from '@/components/service_layout';
+import { GoogleLoginButton } from '@/components/google_login_button';
 
 const IndexPage: NextPage = function () {
-  return <>Hello world</>;
+  return (
+    <ServiceLayout title="test">
+      <Box maxWidth="md" mx="auto">
+        <img src="/main_logo.svg" alt="메인 로고" />
+        <Flex justify="center">
+          <Heading>#BlahBlah</Heading>
+        </Flex>
+      </Box>
+      <Center mt="20">
+        <GoogleLoginButton />
+      </Center>
+    </ServiceLayout>
+  );
 };
 
 export default IndexPage;
